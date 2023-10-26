1bb1553a4326479813cd54bb034c26bd730daeee
laithesaid <148649015+laithesaid@users.noreply.github.com>
2023-10-26 18:35:20 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace EncryptionSolo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Value:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(349, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Encrypt:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(349, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Decrypt:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PasswordChar = '*';
+            this.textBox4.Size = new System.Drawing.Size(349, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(27, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1145, 272);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Encrypt/Decrypt";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(493, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(501, 21);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "DO NOT SHOW ENCRYPTED STRING IN TEXTBOX SEND TO A FOLDER";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(493, 108);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(218, 21);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "ENCRYPTUSINGPASSWORD";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 351);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "EncryptionSolo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
+
+namespace EncryptionSolo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string hash = "ENCryPtIon SolO";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(textBox1.Text); //converting text into a byte array
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider()) //creating an instance of the md5crypto class
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash)); //this returns the has as an array of 16 bytes
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider()
+                { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })    //set the secret key for the tripleDES algorithm
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();    //transform the specified region of bytes array to resultArray
+
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);    //Release resources held by TripleDes Encryptor
+
+                    textBox2.Text = Convert.ToBase64String(results, 0, results.Length);  //prints in second textbox
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string password;
+            password = "1";
+
+            if (checkBox2.Checked == false)
+            {
+
+                //else if (password == textBox4.Text)
+                //  {
+                byte[] data = Convert.FromBase64String(textBox2.Text);
+                //if hashing was used get the hash code with regards to your key
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                    using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider()
+                    { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })//set the secret key for the tripleDES algorithm
+                    {
+                        ICryptoTransform transform = tripDes.CreateDecryptor();
+                        byte[] results = transform.TransformFinalBlock(data, 0, data.Length);    //Release resources held by TripleDes Encryptor  
+                        string decryptresult = UTF8Encoding.UTF8.GetString(results);
+                        //string path = @"C:\Users\laith\Desktop\University First Semester 2020\System Analysis\decryptresult.txt";
+
+                        if (checkBox1.Checked)
+                        {
+                            try
+                            {
+
+                                TextWriter tw = new StreamWriter(@"C:\Users\laith\Desktop\University First Semester 2020\System Analysis\decryptresult.txt", true);
+                                tw.WriteLine(decryptresult);
+                                tw.Close();
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine(e);
+                                throw;
+                            }
+
+                            // File.WriteAllText( path,decryptresult);
+                            // @"C: \Users\laith\Desktop\University First Semester 2020\System Analysis
+                        }
+
+
+                        else
+                        {
+                            textBox3.Text = UTF8Encoding.UTF8.GetString(results); //prints decrypted data in third textbox
+
+                        }
+
+
+                    }
+                }
+            }
+            else if (checkBox2.Checked && (password == null || textBox4.Text != password))
+            {
+                MessageBox.Show("WRONG PASSWORD PLEASE RE ENTER ASSIGNED PASSWORD BY THE COMPANY",
+                                "ERROR",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                textBox4.Text = "";
+            }
+
+
+            else if (checkBox2.Checked && password == textBox4.Text)
+            {
+                byte[] data = Convert.FromBase64String(textBox2.Text);
+                //if hashing was used get the hash code with regards to your key
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                    using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider()
+                    { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })//set the secret key for the tripleDES algorithm
+                    {
+                        ICryptoTransform transform = tripDes.CreateDecryptor();
+                        byte[] results = transform.TransformFinalBlock(data, 0, data.Length);    //Release resources held by TripleDes Encryptor  
+                        string decryptresult = UTF8Encoding.UTF8.GetString(results);
+                        //string path = @"C:\Users\laith\Desktop\University First Semester 2020\System Analysis\decryptresult.txt";
+
+                        if (checkBox1.Checked)
+                        {
+                            try
+                            {
+
+                                TextWriter tw = new StreamWriter(@"C:\Users\laith\Desktop\University First Semester 2020\System Analysis\decryptresult.txt", true);
+                                tw.WriteLine(decryptresult);
+                                tw.Close();
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine(e);
+                                throw;
+                            }
+
+                            // File.WriteAllText( path,decryptresult);
+                            // @"C: \Users\laith\Desktop\University First Semester 2020\System Analysis
+                        }
+
+
+                        else
+                        {
+                            textBox3.Text = UTF8Encoding.UTF8.GetString(results); //prints decrypted data in third textbox
+
+                        }
+
+
+                    }
+                }
+            }
+            }
+
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
